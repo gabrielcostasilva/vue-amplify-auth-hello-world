265b29836bc3aad7e861fe4eb4abff9e1e5077ae
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-03-19 10:24:37 -0300

    - Replaced static values with env variables.

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Amplify from 'aws-amplify';
 Amplify.configure({
     Auth: {
-        region: '',
-        userPoolId: '',
-        userPoolWebClientId: '',
+        region: process.env.VUE_APP_AUTH_REGION,
+        userPoolId: process.env.VUE_APP_USER_POOL_ID,
+        userPoolWebClientId: process.env.VUE_APP_USER_POOL_WEB_CLIENT_ID,
 
         oauth: {
-            domain: 'XXX.auth.us-east-1.amazoncognito.com',
+            domain: process.env.VUE_APP_OAUTH_DOMAIN,
             scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
             redirectSignIn: 'https://www.uol.com.br/',
             redirectSignOut: 'https://www.amazon.com.br/',
